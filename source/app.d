b0370c18f5dd49c092aa4e4d695293b06e930de9
Amaranthos <joshua.hodkinson.42@gmail.com>
2016-11-30 21:45:49 +1000

    Created base.dt, footer.dt, nav.dt. These inherit as needed. Fleshing out connect to mongoDB and auth'ing user

--- source/app.d
+++ source/app.d
@@ -21,7 +21,7 @@ shared static this() {
 	auto settings = new HTTPServerSettings;
 	settings.sessionStore = new MemorySessionStore;
 	settings.port = 8080;
-	settings.bindAddresses = ["::1", "127.0.0.1"];
+	settings.bindAddresses = ["::1", "0.0.0.0"];
 	listenHTTP(settings, router);
 
 	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
--- source/blogd/webapp.d
+++ source/blogd/webapp.d
@@ -3,24 +3,42 @@ module blogd.webapp;
 import vibe.d;
 import blogd.userdata;
 
-class WebApp {
-	private enum auth = before!ensureAuth("_authUser");
+final class WebApp {
+	private enum auth = before!ensureAuth("_user");
 	private SessionVar!(UserData, "user") user;
+	private MongoClient mongo;
 
-	private string ensureAuth(HTTPServerRequest req, HTTPServerResponse res) {
+	this() {
+		mongo = connectMongoDB("localhost");
+	}
+
+	@method(HTTPMethod.GET)
+	void index() {
+		string title = "home";
+		render!("index.dt", title);
+	}
+
+	@method(HTTPMethod.GET)
+	void login(string _error = null) {
+		string title = "login";
+		render!("login.dt", title, _error);
+	}
+
+	@method(HTTPMethod.POST)
+	@errorDisplay!login
+	void login(string email, string password) {
+		MongoCollection users = mongo.getCollection("blogd.users");
+		//enforce(users.findOne(["email": email, "password": password]) != Bson(null), "Incorrect email/password");
+		//auto json = users.findOne(["email": email, "password": password]);
+		string title = "post";
+		render!("test.dt", title, users);
+		//redirect("index");
+	}
+
+	private string ensureAuth(scope HTTPServerRequest req, scope HTTPServerResponse res) {
 		if(!user.loggedIn) {
 			redirect("/");
 		}
 		return user.name;
 	}
-
-	@method(HTTPMethod.GET)	
-	void index(string _error = null) {
-		render!("index.dt", _error);
-	}
-
-	@method(HTTPMethod.POST)	
-	void login(string _error = null) {
-		render!("index.dt", _error);
-	}	
 }
